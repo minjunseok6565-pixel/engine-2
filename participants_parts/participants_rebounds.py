302987b1366c9e4b5a5548e9bf725ee4a2500c23
minjunseok6565-pixel <minjunseok6565@gmail.com>
2026-02-07 20:13:37 +0900

    Update participants_rebounds.py

--- participants_parts/participants_rebounds.py
+++ participants_parts/participants_rebounds.py
@@ -4,8 +4,8 @@ import math
 import random
 from typing import Dict, List, Optional
 
-from .core import weighted_choice
-from .models import Player, TeamState
+from ..core import weighted_choice
+from ..models import Player, TeamState
 
 from .participants_common import _active, _clamp
 
